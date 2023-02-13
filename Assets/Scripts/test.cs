999df0f55e666dabda45d1987d80c78727da381a
eli0504 <2003ecc@gmail.com>
2023-02-13 21:59:22 +0100

    test

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ex4 : MonoBehaviour
+public class test : MonoBehaviour
 {
     public int start = 3;
     public int end = 27;
-    private int i = 1;
+    private int i = 0;
 
     private void Start()
     {
-        while (i > start && i < end)
+        while(start < end)
         {
-            if (i % 5 == 0)
+            if (i % 7 == 0)
             {
                 Debug.Log(i);
-                i += 5;
+                i+=5;
+               
             }
-
             i++;
         }
     }
 }
- 
